02c8681d2be08c4be222f0fd6e2d239788a2c5b8
liuguowei <1085550655@qq.com>
2022-07-29 08:31:43 +0800

    feat: vue3学习

---
+++ documents/vue_document/document/vue3/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{count}}
+  </div>
+</template>
+
+<script>
+import { ref, getCurrentInstance } from 'vue'
+export default {
+  name: 'HelloWorld',
+  setup (props, ctx) {
+    const instance = getCurrentInstance()
+    console.log(instance)
+    const count = ref(0)
+
+    return {
+      count
+    }
+  }
+}
+</script>
---
+++ documents/vue_document/document/vue3/src/views/LifeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="生命周期">
+    <HelloWorld msg="well"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
