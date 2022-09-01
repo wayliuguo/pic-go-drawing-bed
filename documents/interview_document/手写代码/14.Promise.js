6f5ffc0e806543cac531161f070357c13d79f001
liuguowei <1085550655@qq.com>
2022-09-01 20:00:22 +0800

    feat: 手写promise

--- documents/interview_document/手写代码/14.Promise.js
+++ documents/interview_document/手写代码/14.Promise.js
@@ -29,10 +29,21 @@ class myPromise {
             reject(e)
         }
     }
-    then (onFulfilled, onRejected)
+    // then 方法
+    then (onFulfilled, onRejected) {
+        // 如果状态是 fulfilled，则执行then传入的 onFulfilled 函数
+        if (this.state === FULFILLED) {
+            typeof onFulfilled === 'function' && onFulfilled(this.value)
+        }
+        // 如果状态是 fulfilled，则执行then传入的 onRejected 函数
+        if (this.state === REJECTED) {
+            typeof onRejected === 'function' && onRejected(this.reason)
+        }
+    }
 }
 
 const promise = new myPromise ((resolve, reject) => {
     resolve(1)
 })
 console.log(promise)
+promise.then((res) => console.log(res))
