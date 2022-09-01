3f79286723d2ae71801e9bdaaa06f8ad3ff29bf2
liuguowei <1085550655@qq.com>
2022-09-01 17:36:22 +0800

    feat: 手写代码

--- documents/interview_document/手写代码/14.Promise.js
+++ documents/interview_document/手写代码/14.Promise.js
@@ -6,6 +6,33 @@ class myPromise {
     constructor (executor) {
         // 保存 promise 的状态
         this.state = PENDING
-        
+        // 成功结果
+        this.value = undefined
+        // 失败结果
+        this.reason = undefined
+        // resolve 方法
+        const resolve = (value) => {
+            if (this.state = PENDING) {
+                this.state = FULFILLED
+                this.value = value
+            }
+        }
+        const reject = (reason) => {
+            if (this.state = PENDING) {
+                this.state = REJECTED
+                this.reason = reason
+            }
+        }
+        try {
+            executor(resolve, reject)
+        } catch (e) {
+            reject(e)
+        }
     }
+    then (onFulfilled, onRejected)
 }
+
+const promise = new myPromise ((resolve, reject) => {
+    resolve(1)
+})
+console.log(promise)
