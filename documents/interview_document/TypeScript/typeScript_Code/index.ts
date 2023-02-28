d10186ee201808b9b108dc9e53826ee74f0670b0
liuguowei <1085550655@qq.com>
2023-02-28 08:40:02 +0800

    feat: ts 结构类型系统-函数兼容性

--- documents/interview_document/TypeScript/typeScript_Code/index.ts
+++ documents/interview_document/TypeScript/typeScript_Code/index.ts
@@ -1,13 +1,36 @@
 export {}
-class Animal {
-    name!: string
-}
-class Bird extends Animal {
-    age!: number
-}
-let b: Bird = {
-    name: '',
-    age: 0
-}
-const a: Animal = b
 
+// 比较参数: 参数不能比定义的多
+type Func = (a: number, b: number) => void
+let sum: Func
+const f1 = (a: number, b:number):void => {}
+function f2(a:number):void {}
+// sum  有两个number类型参数，只要符合子集即可满足类型
+sum = f1
+sum = f2
+
+// 比较返回值：返回值不能比定义的少
+type GetPerson = () => {name: string, age: number}
+let getPerson: GetPerson
+function g1 () {
+    return {
+        name: 'well',
+        age: 18
+    }
+}
+function g2 () {
+    return {
+        name: 'well'
+    }
+}
+function g3 () {
+    return {
+        name: 'well',
+        age: 18,
+        sex: 1
+    }
+}
+getPerson = g1
+// getPerson = g2 // 类型 "{ name: string; }" 中缺少属性 "age"
+getPerson().age.toString() // 以这个来理解， g2没有age就会报错
+getPerson = g3
