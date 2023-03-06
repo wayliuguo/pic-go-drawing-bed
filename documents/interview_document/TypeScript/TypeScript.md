df55b2f87299d6a79d595444cb94bb5ee0421347
liuguowei <1085550655@qq.com>
2023-03-06 14:04:46 +0800

    feat: ts ReturnType

--- documents/interview_document/TypeScript/TypeScript.md
+++ documents/interview_document/TypeScript/TypeScript.md
@@ -2099,6 +2099,47 @@ let t0: T0 = 'well'
 let tt0: MT0 = 'well'
 ```
 
+##### ReturnType
+
+用来得到一个函数的返回值类型
+
+```
+/**
+ * 自定义实现
+ * 1. infer 在这里用于提取函数类型的返回值
+ * 2.ReturnType<T> 只是将 infer R 从参数位置移动到返回值位置，因此此时 R 即是表示待推断的返回值类型
+ */
+type MyReturnType<T extends (...args: any[]) => any> = T extends (
+    ...args: any[]
+) => infer R ? R : any
+
+function getUser(name: string, age: number) {
+    return {
+        name,
+        age
+    }
+}
+/**
+ * type GetUserType = (name: string, age: number) => {
+    name: string;
+    age: number;
+   }
+ */
+type GetUserType = typeof getUser
+/**
+ * type ReturnUser/MyReturnUser = {
+    name: string;
+    age: number;
+  }
+ */
+type ReturnUser = ReturnType<GetUserType>
+type MyReturnUser = MyReturnType<GetUserType>
+let u: ReturnUser = {
+    name: 'well',
+    age: 18
+}
+```
+
 
 
 ## 11.工具类型
--- documents/interview_document/TypeScript/typeScript_Code/index.ts
+++ documents/interview_document/TypeScript/typeScript_Code/index.ts
@@ -1,8 +1,34 @@
-type A = null | undefined | string
+/**
+ * 自定义实现
+ * 1. infer 在这里用于提取函数类型的返回值
+ * 2.ReturnType<T> 只是将 infer R 从参数位置移动到返回值位置，因此此时 R 即是表示待推断的返回值类型
+ */
+type MyReturnType<T extends (...args: any[]) => any> = T extends (
+    ...args: any[]
+) => infer R ? R : any
 
-type MyNonNullable<T> = T extends null | undefined ? never : T
-type T0 = NonNullable<A> // type T0 = string
-type MT0 = NonNullable<A> // type MT0 = string
-
-let t0: T0 = 'well'
-let tt0: MT0 = 'well'
+function getUser(name: string, age: number) {
+    return {
+        name,
+        age
+    }
+}
+/**
+ * type GetUserType = (name: string, age: number) => {
+    name: string;
+    age: number;
+   }
+ */
+type GetUserType = typeof getUser
+/**
+ * type ReturnUser/MyReturnUser = {
+    name: string;
+    age: number;
+  }
+ */
+type ReturnUser = ReturnType<GetUserType>
+type MyReturnUser = MyReturnType<GetUserType>
+let u: ReturnUser = {
+    name: 'well',
+    age: 18
+}
