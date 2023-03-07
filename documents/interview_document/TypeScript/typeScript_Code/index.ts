ada7498eb722a54ab9601d67c42ebc7ceb0c3cec
liuguowei <1085550655@qq.com>
2023-03-07 21:59:01 +0800

    feat: ts ReadOnly

--- documents/interview_document/TypeScript/typeScript_Code/index.ts
+++ documents/interview_document/TypeScript/typeScript_Code/index.ts
@@ -3,16 +3,19 @@ interface Company {
     name?: string
     salary?: number
 }
-
-/* type RequiredCompany = {
-    id: number;
-    name: string;
-    salary: number;
-} */
-type RequiredCompany = Required<Company>
-
 // 自定义实现
-type MyRequired<T> = {
-    [P in keyof T]-?: T[P]
+type MyReadonly<T> = {
+    readonly [P in keyof T]: T[P];
 }
-type MyRequiredCompany = MyRequired<Company>
+type ReadOnlyPerson = Readonly<Company>
+/* 
+type ReadOnlyPerson = {
+    readonly id: number;
+    readonly name?: string | undefined;
+    readonly salary?: number | undefined;
+}
+*/
+let c: ReadOnlyPerson = {
+    id: 0
+}
+// c.id = 2 // Cannot assign to 'id' because it is a read-only property
