1d9fbaef20e15f9ddff7a3c52776a4be5c5a9ce5
liuguowei <1085550655@qq.com>
2023-03-07 13:45:16 +0800

    ts infer应用

--- documents/interview_document/TypeScript/TypeScript.md
+++ documents/interview_document/TypeScript/TypeScript.md
@@ -2184,7 +2184,33 @@ type params = ConstructorParameters<typeof Person>
 let p: params = ['well']
 ```
 
+##### infer 应用
 
+```
+// tuple(元组)转union(联合)
+type Elements<T> = T extends Array<infer E> ? E : never
+type Ttuple = [string, number]
+// type TupleToUnion = string | number
+type TupleToUnion = Elements<Ttuple>
+
+// 联合类型转交叉类型
+type T1 = {
+    name: string
+}
+type T2 = {
+    age: number
+}
+type ToIntersection<T> = T extends {
+    a: (x: infer U) => void, b: (x: infer U) => void
+} ? U : never
+// type T3 = T1 & T2
+type T3 = ToIntersection<{ a: (x: T1) => void, b: (x: T2) => void}>
+let tt3: T3 = {
+    name: 'well',
+    age: 18
+}
+
+```
 
 ## 11.工具类型
 
--- documents/interview_document/TypeScript/typeScript_Code/index.ts
+++ documents/interview_document/TypeScript/typeScript_Code/index.ts
@@ -1,14 +1,22 @@
-class Person {
+// tuple(元组)转union(联合)
+type Elements<T> = T extends Array<infer E> ? E : never
+type Ttuple = [string, number]
+// type TupleToUnion = string | number
+type TupleToUnion = Elements<Ttuple>
+
+// 联合类型转交叉类型
+type T1 = {
     name: string
-    constructor(name: string) {
-        this.name = name
-    }
-    getName() {
-        console.log(this.name)
-    }
 }
-type MyConstructorParameters<T extends abstract new (...args: any) => any> =
-    T extends abstract new (...args: infer P) => any ? P : never
-type params = ConstructorParameters<typeof Person>
-// let p: [name: string]
-let p: params = ['well']
+type T2 = {
+    age: number
+}
+type ToIntersection<T> = T extends {
+    a: (x: infer U) => void, b: (x: infer U) => void
+} ? U : never
+// type T3 = T1 & T2
+type T3 = ToIntersection<{ a: (x: T1) => void, b: (x: T2) => void}>
+let tt3: T3 = {
+    name: 'well',
+    age: 18
+}
