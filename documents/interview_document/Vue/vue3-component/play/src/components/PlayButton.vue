d51166341fa9daba0ad6c7d3b20ce550333cd3d6
liuguowei <1085550655@qq.com>
2023-04-15 21:38:31 +0800

    feat: button play

--- documents/interview_document/Vue/vue3-component/play/src/components/PlayButton.vue
+++ documents/interview_document/Vue/vue3-component/play/src/components/PlayButton.vue
@@ -3,8 +3,18 @@
     size="medium"
     type="danger"
     :round="true"
-    :loading="true"
-  >
-  按钮
+    :loading="false"
+    :disabled="false"
+    icon-placement="left"
+  > 13246
+    <template #icon>
+      <z-icon>
+        <AddCircle></AddCircle>
+      </z-icon>
+    </template>
   </z-button>
 </template>
+
+<script setup lang="ts">
+import {  AddCircle } from '@vicons/ionicons5'
+</script>
